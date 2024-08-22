8ea86c370abfb9f1bb170b3739e1168daf38dba4
LucasSantino <lsantino541@gmail.com>
2024-08-21 21:21:31 -0300

    exemplo4

--- Aula_04_Programação_Orien_Objetos/exemplo4.js
+++ Aula_04_Programação_Orien_Objetos/exemplo4.js
@@ -1,29 +1,36 @@
 class Animal{
-    constructo(nome, idade){
+    constructor(){
         this.nome = null;
         this.idade = null;
+
     }
 
-    // Método de classe base
-    fazersom(){
-        console.log(`${this.nome} faz um som,`);
+    //Método da classe base
+    fazerSom(){
+        console.log(`${this.nome} faz um som`);
     }
 }
 
-
-// extends herança, a calsse cachorro herda da classe animal algumas cafracteisticas como nome e idade
-class cachorro extends Animal{
-    constructor(nome, idade, raca) {
-        // chama o constructor da classe base
-        super(nome, idade, raca){
-            // Chama o construtor da classe base
-            super(nome, idade);
-            this.raca=raca;
-        }
-        
+//Extends herança, a classe cachorro herda da classe animal algumas características como nome e idade
+class Cachorro extends Animal{
+    constructor(){
+        //Chama o construtor da classe base
+        super();
+        this.raça = null;
     }
-    /// Método Sobrescrito
-    exibirinfo(){
-        console.log(`nome: ${this.nome} late.`)
+    //Método sobrescrito
+    fazerSom(){
+        console.log(`${this.nome} late`);
+    }
+
+    //Método adicional
+    exibirInfo(){
+        console.log(`Nome: ${this.nome}, idade: ${this.idade}, Raça: ${this.raça}`);
     }
 }
+
+const Rocky = new Cachorro();
+Rocky.nome = "Rocky";
+Rocky.idade = "2";
+Rocky.raça = "Amstaff";
+Rocky.exibirInfo();
