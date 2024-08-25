4fde279fb496afefaa5ecdde140f0eb4b1ff4947
LucasSantino <lsantino541@gmail.com>
2024-08-25 18:34:31 -0300

    exercicio8

--- exercicios_Aula03/exercicio8.js
+++ exercicios_Aula03/exercicio8.js
@@ -1,3 +1,10 @@
-let valor = 42; 
-let resultado = (valor % 2 === 0) ? 'Par' : 'Ímpar';
-console.log("O valor " + valor + " é " + resultado + ".");
+const usuarioLogado = true;        
+const PermissaoAdmin = false;    
+
+if (usuarioLogado && PermissaoAdmin) {
+    console.log('Acesso concedido. Você tem permissão de administrador.');
+} else {
+    console.log('Acesso negado. Verifique se você está logado e se possui permissão de administrador.');
+}
+
+
