d03abf086f8df0f9d3b96798853853ec6027a05c
LucasSantino <lsantino541@gmail.com>
2024-08-25 18:43:08 -0300

    exercicios aula 03

---
+++ exercicios_Aula03/exercicio10.js
@@ -0,0 +1,10 @@
+const idadeMinima = 18;
+const idadeUsuario = 20;
+
+// Verifica se o usuário pode comprar o ingresso
+if (idadeUsuario >= idadeMinima) {
+    console.log('Você pode comprar o ingresso para o show.');
+} else {
+    console.log('Você não tem idade suficiente para comprar o ingresso.');
+}
+
