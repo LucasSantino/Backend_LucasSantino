22be6d32e6483484fb653f79ff3f31f551de6742
LucasSantino <lsantino541@gmail.com>
2024-08-25 18:59:24 -0300

    Update exercicio1.js

---
+++ Aula_04_Programação_Orien_Objetos/exercicios_Aula04/exercicio1.js
@@ -0,0 +1,35 @@
+// Definição da classe Carro
+class Carro {
+    // Construtor para inicializar os atributos
+    constructor(marca, modelo, ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.motor_ligado = false;  // Inicialmente, o motor está desligado
+    }
+
+    // ligar o motor
+    ligar_motor() {
+        this.motor_ligado = true;
+    }
+
+    // desligar o motor
+    desligar_motor() {
+        this.motor_ligado = false;
+    }
+
+    // Método para verificar o status do motor
+    status_motor() {
+        return this.motor_ligado ? 'O motor está ligado.' : 'O motor está desligado.';
+    }
+}
+
+// Criando um objeto da classe Carro
+const meuCarro = new Carro('Honda', 'Civic', 2020);
+
+// Testando os métodos
+console.log(meuCarro.status_motor()); 
+meuCarro.ligar_motor();  
+console.log(meuCarro.status_motor()); 
+meuCarro.desligar_motor(); 
+console.log(meuCarro.status_motor()); 
