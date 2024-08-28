e711d3aa99def4a0228a5fc2865538ea82919766
LucasSantino <lsantino541@gmail.com>
2024-08-28 20:44:19 -0300

    exemplo12.

--- Aula_05_Array_Json/exemplo12.js
+++ Aula_05_Array_Json/exemplo12.js
@@ -5,3 +5,9 @@ carro.modelo = "Jaguar"
 carro.ano = 2024
 carro.combustivel = "Hibrido"
 carro. portas = 5
+carro.acelera = function(){
+    return 'O carro está acelerando'
+}
+
+console.log(carro)
+console.log(carro.acelera())
