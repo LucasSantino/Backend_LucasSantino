44e1a7d0cbca2c6153841a56dc9403af0e456695
LucasSantino <lsantino541@gmail.com>
2024-08-28 19:55:11 -0300

    exemplo8 filter

---
+++ Aula_05/exemplo8.js
@@ -0,0 +1,12 @@
+///Exemplo 8 FILTER(filter)
+
+const numeros=[1,2,3,4,5]
+const pares = nomes.filter(num => num %2 === 0);
+    console.log(pares)
+
+
+// forma de calcular o resto de um numero
+num = numeros % 2
+if(num ===0){
+    console.log(num)
+}
