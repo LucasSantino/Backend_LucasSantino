cb5791e9ab5a4bf74865b47e9653a50f720ac759
LucasSantino <lsantino541@gmail.com>
2024-08-28 19:24:20 -0300

    exemplo3 array unshift e pop

---
+++ Aula_05/exemplo3.js
@@ -0,0 +1,15 @@
+notas=[10,9,8,5,6] // cria um array de notas
+console.log(notas) // exeibe o array de notas
+
+notas.unshift(7.7) // UNSHIFT ADICIONA elementos no array na ESQUERDA
+console.log(notas)
+notas.unshift(6.5)
+console.log(notas)
+
+//notas.shift()
+notas=[10,9,8,5,6]
+console.log(notas)
+notas.shift(7.7)   // SHUFT REMOVE elemetos
+console.log(notas)
+notas.pop()
+console.log(notas)
