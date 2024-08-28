2bae68114972b718f3eb78059301a0f61723d0b0
LucasSantino <lsantino541@gmail.com>
2024-08-28 20:18:29 -0300

    .

--- Aula_05_Array_Json/exemplo4Array.js
+++ Aula_05_Array_Json/exemplo4Array.js
@@ -1,9 +1,13 @@
-notas[8,9,7,6]
+notas =[8,9,7,6]
 notas.pop()
 console.log(notas)
-notas.push(9,5)
+notas.push(9.5)
+console.log(notas)
+notas.pop()
 console.log(notas)
 notas.pop()
 console.log(notas.pop())
-notas,push(8)
-console.log()
+notas.push(8)
+console.log(notas)
+notas.push(7.5)
+console.log(notas)
