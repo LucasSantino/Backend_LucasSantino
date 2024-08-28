dbaf123ee84f6166e741d45108f0f72e7bc6efff
LucasSantino <lsantino541@gmail.com>
2024-08-28 19:08:09 -0300

    exemplo 1 array

---
+++ Aula_05/exemplo1.js
@@ -0,0 +1,8 @@
+/// PRIMEIRA FORMA DE CRIAR aRRAY
+const frutas = ['Pera', 'Uva', 'Maça']; // cria um array chamado FRUTAS
+console.log(frutas);    
+console.log(frutas[2])   // ordem numerica do Array (0,1,2,3.....) Ou seja, ( Pera, Uva, Maça ....)
+
+/// SEGUNDA FORMA DE CRIAR ARRAY
+const frutas2 = new Array('Maça', 'Morango', 'Laranja') 
+console.log(frutas2[1]);   /// [] colchetes utilizados para indicar a posição
