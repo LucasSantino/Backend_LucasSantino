5933eca4e659bda2c57dd6342bbff17a05dcead3
LucasSantino <lsantino541@gmail.com>
2024-08-28 19:14:39 -0300

    exemplo2 array pop e push

---
+++ Aula_05/exemplo2.js
@@ -0,0 +1,10 @@
+notas =[5,10,7.5];
+console.log(notas);
+notas.pop(); /// pop REMOVE um elemento do array
+console.log(notas);
+notas.pop(); /// pop REMOVE um elemento do array
+console.log(notas);
+notas.push(9) /// push ADICIONA um elemento no array
+console.log(notas);
+notas.push(10) /// push ADICIONA um elemento no array
+console.log(notas)
