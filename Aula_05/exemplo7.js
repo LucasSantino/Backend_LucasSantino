9cae7df19a19729d84dfd2a7d17769a47e4088cc
LucasSantino <lsantino541@gmail.com>
2024-08-28 19:49:50 -0300

    exemplo 7 map

---
+++ Aula_05/exemplo7.js
@@ -0,0 +1,5 @@
+/// EXEMPLO 7 MAP(map)
+
+const numeros = [1,2,3,4]
+const quadrados = numeros.map(num => num*num) /// MAP(map) é uma função que aploca na lista para elevar os numeros ao quadrodos(variavel criado para esse exemplo)
+console.log(quadrados)
