fd038fff18782fc81a183e7a74a7cbe90d374b0b
LucasSantino <lsantino541@gmail.com>
2024-08-28 22:43:24 -0300

    exercicio2

---
+++ Aula_05_Array_Json/Exercicios_Aula05/exercicio2.js
@@ -0,0 +1,15 @@
+//Exemplo 2
+
+const MeuArray =[]; // criação do array
+console.log(MeuArray);
+
+MeuArray.push(10,20,30)
+console.log(`Adicionando numero inteiro:`, MeuArray); // Adicionando numeros inteiros
+
+MeuArray.shift(0)   // SHUFT REMOVE elemetos
+console.log(`Removendo primeiro numero inteiro: `, MeuArray)
+
+MeuArray.unshift(20) // UNSHIFT ADICIONA elementos no array na ESQUERDA
+console.log(MeuArray)
+
+
