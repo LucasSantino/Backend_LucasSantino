1b92c72a0c90dd41d83020d98bc39f75309e86db
LucasSantino <lsantino541@gmail.com>
2024-08-28 21:27:13 -0300

    Exemplp14 Objeto Tipo Boleano

---
+++ Aula_05_Array_Json/exemplo14.js
@@ -0,0 +1,15 @@
+//Exemplo
+
+let usuario={
+    nome: 'Daniel',
+    idade: 29,
+    isAdmimin: true
+}
+
+if(usuario.isAdmimin===true){
+    console.log("O usuario é administrador")
+}
+
+else {
+    console.log("O usuario é administrador")
+}
