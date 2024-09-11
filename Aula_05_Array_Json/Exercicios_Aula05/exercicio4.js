9a7dbe86f13861d3496ffa2bd2bc98486569aaec
LucasSantino <lsantino541@gmail.com>
2024-09-11 20:13:17 -0300

    Exercicios da aula 5 finalizados com sucesso.

--- Aula_05_Array_Json/Exercicios_Aula05/exercicio4.js
+++ Aula_05_Array_Json/Exercicios_Aula05/exercicio4.js
@@ -1,11 +1,22 @@
-Clinica=[];
-console.log("Fila de espera vazia:", Clinica);
-Clinica.push("Pastor Alemão", "Pastor Belga", "Labrador");
-console.log("Chegada de três animais a fila de recepção:", Clinica);
-Clinica.pop("Pastor Alemão");
-console.log("Remoção", Clinicap);
-Clinica.pop("Pastor Belga");
-console.log("Remoção:",Clinica);
-Clinica.pop("Labrador");
-console.log("Remoção", Clinica);
-console.log("Fila de espera vazia: ", Clinica);
+const clinica = [];
+console.log("Fila de espera vazia:", clinica); // Inicializa um array vazio para a fila de espera
+
+
+clinica.push("Pastor Alemão", "Pastor Belga", "Labrador"); // Simula a chegada de três animais
+console.log("Chegada de três animais à fila de recepção:", clinica);
+
+
+const removido1 = clinica.pop(); // Remove os animais da fila um por um
+console.log(`Remoção: ${removido1}`);
+console.log("Fila de espera atual:", clinica);
+
+const removido2 = clinica.pop();
+console.log(`Remoção: ${removido2}`);
+console.log("Fila de espera atual:", clinica);
+
+const removido3 = clinica.pop();
+console.log(`Remoção: ${removido3}`);
+console.log("Fila de espera atual:", clinica);
+
+// Exibe o estado final da fila de espera
+console.log("Fila de espera vazia:", clinica);
