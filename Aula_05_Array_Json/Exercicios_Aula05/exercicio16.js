a1f9ff7cea17f2ca783efdbafe315f6b47a18924
LucasSantino <lsantino541@gmail.com>
2024-09-11 19:16:03 -0300

    Update exercicio16.js

---
+++ Aula_05_Array_Json/Exercicios_Aula05/exercicio16.js
@@ -0,0 +1,30 @@
+const pessoas = [
+    {
+        nome: "Kauã Layon Lima Bonfim",
+        idade: 18,
+        cidade: "Campinas"
+    },
+    {
+        nome: "Lucas Santino da Silva",
+        idade: 22,
+        cidade: "Monte Mor"
+    },
+    {
+        nome: "Luiz Eduardo Bigoli e Silva",
+        idade: 28,
+        cidade: "Hortolandia"
+    },
+    {
+        nome: "Victor dos Santos Ribeiro",
+        idade: 35,
+        cidade: "Campinas"
+    },
+    {
+        nome: "Camilly Rodrigues Lima",
+        idade: 22,
+        cidade: "Monte Mor"
+    }
+];
+
+
+console.log(pessoas);
