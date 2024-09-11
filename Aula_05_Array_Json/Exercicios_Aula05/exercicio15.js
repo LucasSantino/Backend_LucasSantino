7ce8f02882c0f9419a5137095c083a6d0f1af9fc
LucasSantino <lsantino541@gmail.com>
2024-09-11 19:08:43 -0300

    exercicio15

--- Aula_05_Array_Json/Exercicios_Aula05/exercicio15.js
+++ Aula_05_Array_Json/Exercicios_Aula05/exercicio15.js
@@ -2,15 +2,22 @@ const pessoa = {
     nome: "Lucas Santino da Silva",
     idade: 22,
     solteiro: true,
-    hobbies: ["Volei", "VideoGames", "Leitura", "Assistir Filmes"]
+    hobbies: ["Volei", "VideoGames", "Leitura", "Assistir Filmes"],
+    endereco: {
+        rua: "Rua 17, José Pinto Rodrigues",
+        cidade: "Monte Mor",
+        estado: "SP"
+    }
 };
 
+// Modificando a função mostrarInfoPessoa para incluir informações de endereço
 function mostrarInfoPessoa(pessoa) {
     console.log("Nome:", pessoa.nome, "(Tipo:", typeof pessoa.nome, ")");
     console.log("Idade:", pessoa.idade, "(Tipo:", typeof pessoa.idade, ")");
     console.log("Solteiro:", pessoa.solteiro, "(Tipo:", typeof pessoa.solteiro, ")");
     console.log("Hobbies:", pessoa.hobbies, "(Tipo:", typeof pessoa.hobbies, ")");
+    console.log("Endereço:", `Rua: ${pessoa.endereco.rua}, Cidade: ${pessoa.endereco.cidade}, Estado: ${pessoa.endereco.estado}`, "(Tipo:", typeof pessoa.endereco, ")");
 }
 
-
+// Chamando a função para verificar se as informações estão corretas
 mostrarInfoPessoa(pessoa);
