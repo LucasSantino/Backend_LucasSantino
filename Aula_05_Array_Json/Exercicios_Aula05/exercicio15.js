98296adb26c1b4c8f233a7236c2a15189d36eb7a
LucasSantino <lsantino541@gmail.com>
2024-09-11 19:05:28 -0300

    exercicio 15

---
+++ Aula_05_Array_Json/Exercicios_Aula05/exercicio15.js
@@ -0,0 +1,16 @@
+const pessoa = {
+    nome: "Lucas Santino da Silva",
+    idade: 22,
+    solteiro: true,
+    hobbies: ["Volei", "VideoGames", "Leitura", "Assistir Filmes"]
+};
+
+function mostrarInfoPessoa(pessoa) {
+    console.log("Nome:", pessoa.nome, "(Tipo:", typeof pessoa.nome, ")");
+    console.log("Idade:", pessoa.idade, "(Tipo:", typeof pessoa.idade, ")");
+    console.log("Solteiro:", pessoa.solteiro, "(Tipo:", typeof pessoa.solteiro, ")");
+    console.log("Hobbies:", pessoa.hobbies, "(Tipo:", typeof pessoa.hobbies, ")");
+}
+
+
+mostrarInfoPessoa(pessoa);
