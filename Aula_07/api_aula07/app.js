63d0d65e04912cb1a6d89e121b4168f129d14704
LucasSantino <lsantino541@gmail.com>
2024-09-11 22:47:49 -0300

    api aula07

---
+++ Aula_07/api_aula07/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bookRoutes = require('./routes/bookRoutes');
+
+const app = express();
+app.use(bodyParser.json());
+app.use('/api', bookRoutes);
+
+// Definir a porta do servidor
+const PORT = process.env.PORT || 3000;
+
+// Iniciar o servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
