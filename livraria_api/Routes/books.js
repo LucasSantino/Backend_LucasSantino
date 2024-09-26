9f4450718379650c089e91aba02adb38df5133a4
LucasSantino <lsantino541@gmail.com>
2024-09-25 21:22:06 -0300

    .

--- livraria_api/Routes/books.js
+++ livraria_api/Routes/books.js
@@ -19,3 +19,36 @@ try{
 
 });
 
+// Rota Get
+router.get('/', async (req, res)=>{
+    try{
+        const books = await Books.find(); // Busca todos os livros metodo FIND
+        res.status(200).json(books) // retorna a lista de livros
+}catch(error){
+    res.status(500).json({message: 'Erro ao buscar os livros', error}) // retorna o erro se houver
+}
+});
+
+// put - modifica o elemento
+router.put('/:id', async(req, res)=>{
+    const {title, author,year} = req.body; // extrai o conteudo da requisição
+try{
+const updatedBook = await Book.findByIdAndUpdate(req. params.id,{title, author, year},{new: true});
+res.status(200).json(updatedBook);
+}catch(error){
+    res.status(500).json({message: 'erro ao atualizar o livro', error});
+}
+})
+
+// delete - deleta o lemento pelo id
+routwr.delete('/:id',async(req, res)=>{
+    try{
+        await Book.findByIdAndDelete(rq.params.id);
+        res.status(200).json({message: 'Livro deletado com sucesso'});
+    }catch(error){
+        res.status(500).json({message: 'Erro ao deletar livro', error});
+    }
+})
+
+// exporta o roteador para uaar no servidor.js
+modeule.exports = router
---
+++ livraria_api/Server.js
@@ -0,0 +1,26 @@
+// arquivo principal da api
+// inicia o servidor
+
+ const express = require('express')
+ const mongoose = require('mongoose')
+ const cors = require('cors')
+
+ // inicialização do app
+
+ const app = express();
+ app.use(cors());
+ app.use(express.json());
+
+ // Conexão com o mongo db
+ mongoose.connect('mongodb+srv://SantinoLucas:relampago55@library.fetwz.mongodb.net/', {
+    useNewUrlParser:true,
+    useUnifieldTopology:true
+ }).then(()=> console.log('Mongodb conectado')).catch(err=>console.error('Erro ao conectar no mongo',err));
+
+ // Imporatação das rotas criadas
+ const bookRoutes= require('./Routes/books'); 
+ app.use('api/books',bookRoutes); // irá retornar a rota dos livros
+ // Define a porta do servidor
+ app.listen(3000,()=>{
+    console.log('Servidor executando na porta 3000');
+ });
